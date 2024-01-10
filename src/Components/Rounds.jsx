0e76e5772d3edd8f60e0fa6edf55651467deaae7
Gokul Lekhwar <107258618+iamgokx@users.noreply.github.com>
2024-01-10 20:46:43 +0530

    Delete src directory

--- src/Components/Rounds.jsx
+++
@@ -1,9 +0,0 @@
-
-function Rounds({ handleRoundClick, rounds }) {
-  return (
-    <p className="rounds" id="rounds" onClick={handleRoundClick}>
-      {rounds}
-    </p>
-  );
-}
-export default Rounds;
